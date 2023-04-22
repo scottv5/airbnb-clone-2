f4b3c3d84afa93f2dee2d77f22b7f855d145acd7
Scott Varieur <savarieur2022@gmail.com>
2023-04-22 14:53:30 -0700

    add navbar

---
+++ app/components/navbar/UserMenu.tsx
@@ -0,0 +1,47 @@
+"use client";
+import Avatar from "../Avatar";
+import MenuItem from "./MenuItem";
+
+import { AiOutlineMenu } from "react-icons/ai";
+import { useState } from "react";
+
+const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <div className="relative">
+      <div className="flex flex-row items-center gap-3">
+        <div
+          onClick={() => {}}
+          className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
+        >
+          Airbnb your home
+        </div>
+        <div
+          onClick={toggleOpen}
+          className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
+        >
+          <AiOutlineMenu />
+          <div className="hidden md:block">
+            <Avatar />
+          </div>
+        </div>
+      </div>
+      {isOpen ? (
+        <div className="absolute rounded-xl shadow-md w-[4vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
+          <div className="f lex flex-col cursor-pointer">
+            <>
+              <MenuItem onClick={() => {}} label="Login" />
+              <MenuItem onClick={() => {}} label="Sign up" />
+            </>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default UserMenu;
